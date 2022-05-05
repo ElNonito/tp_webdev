c3b375f956758019b738d4b30b22b56eafdda278
Arnaud <arnaud martin>
2022-05-05 18:41:34 +0200

    installed no demon added first html page to epfbook

--- epfbook/app.js
+++ epfbook/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+
 const fs = require("fs")
+const path = require("path");
+
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Helloo World!')
+  res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
 app.get('/students', (req,res) => {
