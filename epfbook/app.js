fc0829820287849a1c10edc681ea7ad41f44a9a3
Arnaud <arnaud martin>
2022-05-05 21:59:39 +0200

    make the create student api work again

--- epfbook/app.js
+++ epfbook/app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.json())
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"))
@@ -42,6 +43,7 @@ app.get("/students/create", (req, res) => {
 
 app.post("/students/create", (req,res) => {
   console.log(req.body)
+  res.redirect("/students/create?created=1")
 });
 
 app.get('/api/students', (req,res) => {
@@ -51,13 +53,15 @@ app.get('/api/students', (req,res) => {
   });
 } )
 
+// TODO : vérifier si elle marche ou pas et la réparer si besoin
 app.post('/api/students/create',(req,res) => {
   console.log(req.body)
-  const csvLine =   `${req.body.name},${req.body.school}\n`
-  fs.appendFile('test_writtingfiles.csv', csvLine, (err) => {
+  const csvLine =   `\n${req.body.name},${req.body.school}`
+  fs.appendFile('stub_database.csv', csvLine, (err) => {
     if (err) throw err;
   });
   res.send('Student created (stub)')
+ 
 } )
 
 app.listen(port, () => {
